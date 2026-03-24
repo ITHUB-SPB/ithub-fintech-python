8a6a992e6cb2d39d12bdbdf6244d2d4894ed1333
Lex Trofimov <daslef93@gmail.com>
2026-03-24 16:24:58 +0300

    24.03

--- landing/urls.py
+++ landing/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from .views import home_view
+from .views import home_view, about_view, contacts_view
 
 urlpatterns = [
     path('', home_view),
-    path('admin/', admin.site.urls),
+    path('about', about_view),
+    path('contacts', contacts_view)
 ]
--- landing/views.py
+++ landing/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'index.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def contacts_view(request):
+    return render(request, 'contacts.html')
