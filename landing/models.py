b77737d51d7bfb8e82e00a90f73c5ad14a7fb361
Lex Trofimov <daslef93@gmail.com>
2026-04-14 14:51:41 +0300

    рефак

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class RecordType(models.Model):
+    title = models.CharField(max_length=30)
+
+
+class Record(models.Model):
+    text = models.TextField(null=True)
+    phone = models.CharField(max_length=15)
+    preferred_timing = models.TimeField()
+    record_type = models.ForeignKey(RecordType, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
