4bee28d6c4549fea5eb9f35b579f4818109f6d01
Lex Trofimov <daslef93@gmail.com>
2026-04-14 16:20:51 +0300

    14:04

--- landing/urls.py
+++ landing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_view, about_view, contacts_view
+from .views import home_view, about_view, contacts_view, create_view
 
 urlpatterns = [
     path('', home_view, name="home"),
+    path('create', create_view, name="create"),
     path('about', about_view, name="about"),
     path('contacts', contacts_view, name="contacts")
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
-from .models import RecordType
+from django.shortcuts import render, redirect
+from .models import RecordType, Record
+
 
 def home_view(request):
-    return render(request, 'index.html')
+    record_types = RecordType.objects.all()
+    records = Record.objects.all()
+
+    return render(
+        request,
+        'index.html',
+        {
+            "record_types": record_types,
+            "records": records
+        }
+    )
+
+
+def create_view(request):
+    text = request.POST.get('text')
+    phone = request.POST.get('phone')
+    preferred_timing = request.POST.get('preferred_timing')
+    record_type_id = int(request.POST.get('record_type'))
+
+    record_type = RecordType.objects.get(pk=record_type_id)
+
+    Record(
+        text=text,
+        phone=phone,
+        preferred_timing=preferred_timing,
+        record_type=record_type
+    ).save()
+
+    return redirect('/')
+
 
 def about_view(request):
     return render(request, 'about.html')
